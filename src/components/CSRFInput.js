4514b5adcf38346867b2d88d3bad38cdf6c66e55
Tom Stanley <tom.stanley37@gmail.com>
2021-05-26 13:50:08 +0100

    Choo choo

---
+++ src/components/CSRFInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CSRFInput = ({ token }) => {
+	return <input type="hidden" name="_token" value={token} />;
+};
+
+CSRFInput.propTypes = {
+	token: PropTypes.string.isRequired(),
+};
+
+export default CSRFInput;
